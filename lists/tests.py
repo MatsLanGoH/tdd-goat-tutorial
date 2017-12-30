6be8f1c8716e4983346edaf3aab7017009a6cb20
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2017-12-30 16:07:16 +0900

    Add unique url stub and test

--- lists/tests.py
+++ lists/tests.py
@@ -25,7 +25,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
@@ -60,4 +60,16 @@ class ItemModelTest(TestCase):
 
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class ListViewTest(TestCase):
+
+    def test_display_all_items(self):
+        Item.objects.create(text='item one')
+        Item.objects.create(text='item zwei')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'item one')
+        self.assertContains(response, 'item zwei')
 
